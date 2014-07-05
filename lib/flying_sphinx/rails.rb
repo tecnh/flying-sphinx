d3699156484b43d04c9eea04f37d989adab97b21
Mat Ellis <mat@cloudability.com>
2014-07-04 18:48:02 -0700

    ActionController:Dispatcher is deprecated after Rails 3.0.9

--- lib/flying_sphinx/rails.rb
+++ lib/flying_sphinx/rails.rb
@@ -1,7 +1,5 @@
-require 'action_controller/dispatcher'
-
 if ENV['FLYING_SPHINX_IDENTIFIER'] || ENV['STAGED_SPHINX_IDENTIFIER']
-  ActionController::Dispatcher.to_prepare :flying_sphinx do
+  Rails.configuration.to_prepare :flying_sphinx do
     FlyingSphinx::Binary.load
   end
 end
